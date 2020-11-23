aa5578d905650a7d9b2fdd34e1b4bfd219d7c457
Rares Sfirlogea <rrr.junior@gmail.com>
2020-11-23 11:46:30 +0100

    Debug check update

--- lib/index.rb
+++ lib/index.rb
@@ -58,6 +58,9 @@ def update_check(id, conclusion, output)
   path = "/repos/#{@owner}/#{@repo}/check-runs/#{id}"
 
   resp = http.patch(path, body.to_json, @headers)
+  puts path
+  puts body
+  puts @headers
 
   raise resp.message if resp.code.to_i >= 300
 end
